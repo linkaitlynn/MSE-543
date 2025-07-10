f9df04b0776ae58d7756758c3659947bca2dfac0
Chris Wang <chris92563@outlook.com>
2025-07-09 22:57:27 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Products from './pages/Products';
@@ -119,5 +119,4 @@ function App() {
   );
 }
 
-// Change this from export default Home to:
 export default App;
